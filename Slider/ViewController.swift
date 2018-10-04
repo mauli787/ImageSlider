912cd6f417f0cbf3de2139741c5b21df27c9086c
DShindeNavsoft <43599397+DShindeNavsoft@users.noreply.github.com>
2018-10-04 16:10:36 +0530

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Sandeep Palod on 04/10/18.
+//  Copyright © 2018 Sandeep Palod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
